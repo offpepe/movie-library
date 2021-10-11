55f58118afea21a030f76717aad04bb805423a55
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-11 17:37:28 -0400

    feat: button to add Movie

--- src/pages/Library.js
+++ src/pages/Library.js
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Card, Col, Container, Row, Button } from 'react-bootstrap';
 import movieContext from '../context/movieContext';
 import { getMovies } from '../services/apiRequests';
 import MovieCard from '../components/MovieCard';
+import { Link } from 'react-router-dom';
 
 export default function Library () {
     const { movieList, setMovieList } = useContext(movieContext);
@@ -19,6 +20,15 @@ export default function Library () {
           </Col>
           <Row xs={1} md={2} className="g-4">
             { movieList && movieList.map((movie) => <MovieCard movie={ movie } />) }
+          </Row>
+          <Row style={ { width: '200px' } }>
+            <Button
+              variant="info"
+            >
+              <Link to="/movies/create">
+              Adicionar novo filme
+              </Link>
+            </Button>
           </Row>
         </Container>
     );
