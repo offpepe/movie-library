b5bf6682c4579ba42de682b05271e0ea6f49c10f
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-10 21:22:30 -0400

    feat: providers and routes on app.js, now w/solid structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import UserProvider from './context/userProvider';
+import MovieProvider from './context/movieProvider';
+import Routes from './services/routes';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider>
+      <MovieProvider>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </MovieProvider>
+    </UserProvider>
   );
 }
 
-export default App;
