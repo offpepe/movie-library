31dd9841ce66dbe2e671c0113d42e7cd6dc63a56
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-15 18:28:59 -0400

    feat: dropDown to user card or login card

--- src/components/LibraryHeader.jsx
+++ src/components/LibraryHeader.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { Dropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { Button, ButtonGroup } from 'react-bootstrap';
+import userContext from '../context/userContext';
+import { getUserByEmail } from '../services/apiRequests';
+import LoginForm from './LoginForm';
+import UserCard from './UserCard';
 
 export default function LibraryHeader () {
+  let { email, setToken, setEmail } = useContext(userContext);
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userData = await getUserByEmail(email);
+      setUser(userData.result);
+    }
+    fetchUser();
+  },[email]);
+  if (!email) {
+    email = localStorage.getItem('email');
+  }
+
+  const loggout = () => {
+    setToken('');
+    setEmail('');
+    localStorage.clear();
+    window.location.reload();
+  }
+
     return (
         <header className="library-header">
             <div className="logo-conteiner" style={ { color: 'yellow', fontSize: '30px' } }>
@@ -11,23 +35,19 @@ export default function LibraryHeader () {
             <div style={ { color: 'white' } } >
                 <h1> Movie Library </h1>
             </div>
-            <ButtonGroup>
-              <Button
-                variant="light"
-              >
-                <Link to="/movies/create" style={ { textDecoration: 'none', color: 'black' } } >
-                  <h5><i class="fas fa-plus"/></h5>
+            <nav className="button-group">
+                <Link to="/movies/create" style={ { textDecoration: 'none' } } >
+                  <h5 style={ { color: 'white' } }><i class="fas fa-plus"/></h5>
                 </Link>
-              </Button>
-              <Button
-                variant="light"
-              >
-                <Link to="/users" style={ { textDecoration: 'none', color: 'black' } } >
-                 <h5><i class="fas fa-user-circle"/></h5>
-                </Link>
-              </Button>
-
-            </ButtonGroup>
+                <Dropdown>
+                  <Dropdown.Toggle variant="dark" id="dropdown-basic">
+                    <h5 style={ { color: 'white' } }><i class="fas fa-user-circle"/></h5>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu className="userMenu">
+                { user ? <UserCard user={ user } loggout={ loggout } /> : <LoginForm setToken={ setToken } setEmail={ setEmail } /> }
+                  </Dropdown.Menu>
+                  </Dropdown> 
+            </nav>
         </header>
     );
 }
