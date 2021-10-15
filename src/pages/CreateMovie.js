e07fd53dcebfa7e104ab98c60d1a36e9f7549197
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-14 20:30:04 -0400

    feat: releaseDate key name

--- src/pages/CreateMovie.js
+++ src/pages/CreateMovie.js
@@ -1,18 +1,25 @@
 import React, { useContext, useState } from 'react';
-import { Container, Col, Form, Button, Alert  } from 'react-bootstrap';
+import { Container, Col as Row, Form, Button, Alert  } from 'react-bootstrap';
 import { Redirect } from 'react-router';
+import StarRatingComponent from 'react-star-rating-component';
 import MovieCardPreview from '../components/MovieCardPreview';
 import userContext from '../context/userContext';
 import { createNewMovie } from '../services/apiRequests';
 import './css/CreateMovie.css';
 
 export default function CreateMovie () {
-    const { token } = useContext(userContext);
+    let { token, email } = useContext(userContext);
+    if (!token || !email) {
+      token = localStorage.getItem('token');
+      email = localStorage.getItem('email');
+    }
     const [newMovie, setNewMovie] = useState({});
     const [dispatchCard, setDispatch] = useState(false);
     const [redirect, setRedirect] = useState(false);
     const [alert, setAlert] = useState({});
     const [inputError, setInputError] = useState(false);
+    const [rate, setRate] = useState(1);
+    const [movieGenres] = useState(['Animação', 'Comédia', 'Comédia Romântica', 'Documentário', 'Drama', 'Faroeste', 'Ficção Científica', 'Musical', 'Suspense', 'Terror / Horror']);
     const submitNewMovie = async () => {
         const operation = await createNewMovie(newMovie, token);
         setAlert(operation);
@@ -26,27 +33,34 @@ export default function CreateMovie () {
         setAlert({});
     }
 
-
+    const date = new Date()
+    const formated = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`
+    const today = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
 
     return (
     <>
       <Container>
-          <Col>
+          <Row>
             <h1> Adicionar novo filme </h1>
-          </Col>
-          <Col>
+          </Row>
+          <Row>
               <Form onSubmit={ (ev) => {
                   ev.preventDefault();
-                  const { title, subtitle, description, cover } = ev.target;
-                  if (!title.value || !subtitle.value || !description.value || !cover.files[0]) {
+                  const { title, subtitle, description, genre, releaseDate, rate, cover } = ev.target;
+                  if (!title.value || !subtitle.value || !genre.value || !releaseDate.value || !rate.value || !description.value || !cover.files[0]) {
                       setInputError(true);
                   } else {
                   setNewMovie({
                       title: title.value,
                       subtitle: subtitle.value,
+                      genre: genre.value,
+                      releaseDate: releaseDate.value,
+                      rate: rate.value,
                       description: description.value,
                       cover: cover.files[0],
-                  });
+                      createdBy: email,
+                      createdAt: today,
+                  });       
                   setDispatch(true);
                   }
               } }>
@@ -59,16 +73,30 @@ export default function CreateMovie () {
                   </Form.Label>
                   <Form.Control type="text" name="subtitle" />
                   <Form.Label>
+                      Gênero
+                  </Form.Label>
+                  <Form.Select name="genre">
+                    { movieGenres.map((genre) => <option value={ genre }>{ genre }</option>) }
+                  </Form.Select>
+                  <Form.Label>
+                      Ano de Lançamento
+                  </Form.Label>
+                  <Form.Control type="date" name="releaseDate" max={ formated } min='1888-12-12' />
+                  <Form.Label> Avaliação </Form.Label>
+                  <Row style={ { fontSize: "30px" } }>
+                  <StarRatingComponent name="rate" value={ rate } starCount={ 5 } onStarClick={ setRate } />
+                  </Row>
+                  <Form.Label>
                       Descrição
                   </Form.Label>
-                  <Form.Control type="textarea" name="description" />
+                  <Form.Control as="textarea" name="description" style={{ height: '100px' }} />
                   <Form.Label>
                       Capa
                   </Form.Label>
                   <Form.Control type="file" name="cover" />
                     <Button variant="secondary" type="submit"> Adicionar </Button>
               </Form>
-          </Col>
+          </Row>
           { inputError  && <Alert variant="warning" onClose={ () => setInputError   (false) } dismissible >
               <Alert.Heading> Todos os campos precisam ser preenchidos </Alert.Heading>
               </Alert>}
