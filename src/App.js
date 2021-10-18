afd7fb2524e1b617b9c1a68bf320d15f09e946d7
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-18 15:08:41 -0400

    fix: index.js structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import UserProvider from './context/userProvider';
-import MovieProvider from './context/movieProvider';
 import Routes from './services/routes';
 
 export default function App() {
   return (
-    <UserProvider>
-      <MovieProvider>
-        <BrowserRouter>
           <Routes />
-        </BrowserRouter>
-      </MovieProvider>
-    </UserProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import UserProvider from './context/userProvider';
+import MovieProvider from './context/movieProvider';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <UserProvider>
+    <MovieProvider>
+     <BrowserRouter>
+       <App />
+      </BrowserRouter>
+    </MovieProvider>
+  </UserProvider>,
   document.getElementById('root')
 );
 
