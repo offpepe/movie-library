6f3d74864e7e8afaccd453fc0af444d223adc641
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-18 15:31:36 -0400

    i wrote word instead world lol

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import Routes from './services/routes';
+import CreateMovie from './pages/CreateMovie';
+import Library from './pages/Library';
+import SignIn from './pages/SignIn';
+import Credits from './pages/Credits';
+import { Route, Switch } from 'react-router';
 
 export default function App() {
   return (
-          <Routes />
+    <Switch>
+      <Route exact path="/"><Library /></Route>
+      <Route exact path="/signIn"><SignIn /></Route>
+      <Route exact path="/create"><CreateMovie /></Route>
+      <Route exact path="/credits"><Credits /></Route>
+    </Switch>
   );
 }
 
