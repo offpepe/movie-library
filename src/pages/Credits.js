a43f9c09bb087d1448762b72873b0eb730d9f60f
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-18 09:09:03 -0400

    feat: credits page

---
+++ src/pages/Credits.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './css/Credits.css';
+
+export default function Credits () {
+        return (
+            <main className="credits">
+              <h1> Movie Library </h1>
+              <section className="credits-article">
+                  <h5> Obrigado por testar minha aplicação ❤</h5>
+                  <article>
+                      Aplicação completamente feita por Alan Albuquerque Ferreira Lopes. Esta aplicação faz parte de um projeto com intuíto didático, chama-se Trybe NW+!
+                      Resumidamente, Trybe NW+ é onde farei todos os projetos da Trybe desde o Módulo de Front-End.
+                  </article>
+              </section>
+              <nav className="credits-contact">
+                  <span><a href="https://www.linkedin.com/in/alanalbuquerq/" target="_blank" rel="noreferrer">
+                      <i class="fab fa-linkedin-in" />
+                    </a></span>
+                  <span><a href="http://github.com/offpepe/" target="_blank" rel="noreferrer">
+                      <i class="fab fa-github" />
+                  </a></span>
+              </nav>
+            </main>
+        );
+}
+
+ 
